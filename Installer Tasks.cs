dc2c4d8892ac4525ffec899c5ca35df0996f26cb
ZapTech43 <30797591+ZapTech43@users.noreply.github.com>
2018-07-03 11:59:21 +0700

    Make sure to complete the logics (errors, warning, etc.)

---
+++ Installer Tasks.cs
@@ -0,0 +1,7 @@
+#Installer Logics for the program
+#DO NOT MODIFY, INTERCEPT OR DITRIBUTE IT WITHOUT USING THE MIT License Rights
+
+using DataFlow.Engine.Primary.Installer
+using DataFlow.Installer.Engine
+using DataFlow.Installer.Extractor
+using DataFlow.Installer.Cleaner
